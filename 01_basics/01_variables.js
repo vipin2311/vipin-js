ff7fc7a5eb0a0816f7a1f54fc5e60b71c9951a9b
vipin2311 <kumarvipin55512@gmail.com>
2024-12-05 12:46:47 +0000

    variables done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId =12345
+let accountEmail = "vipin@gamil.com"
+var accountPassword = "2345"
+accountCity = "bhopal"
+let accountState;
+
+accountEmail = "kumar@gamil.com"
+accountPassword = "6786"
+accountCity = "patna"
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+//prefer not use var
+//because of issue in block scope and functional scope
