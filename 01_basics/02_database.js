5cbe2b093c9d11138b0a181283f8045d6bc15920
vipin2311 <kumarvipin55512@gmail.com>
2024-12-07 11:56:07 +0000

    datatypes

---
+++ 01_basics/02_database.js
@@ -0,0 +1,13 @@
+"use strict" //treat all js code  as new version
+
+//alert("hello") // we are using node js not browser
+
+console.log(3+3);
+
+console.log("vipin")
+
+let mame = "vipin"
+let age = 18
+let inloggedin = true
+
+console.log(typeof null);
