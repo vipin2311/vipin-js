b767cda0f1183deda15fcbbe3ef46afec7b9ddd8
vipin2311 <kumarvipin55512@gmail.com>
2024-12-07 12:47:39 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = null
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33"= 33
+//"33ads" = nan
+// true =1, false =0;
+
+let isLoggedIn = "vipin"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+//1 = true
+// "" =false
+//" vipin" = true
+
+ let number = 33
+
+ let stringNumber =String(number)
+ console.log(stringNumber);
+ console.log(typeof stringNumber)
