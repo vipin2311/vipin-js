58273b4d2e0d7949c7f7d6e1bb582c1825f53ae4
vipin2311 <kumarvipin55512@gmail.com>
2024-12-25 15:37:54 +0000

    comparision.js

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,21 @@
+
+// console.log(2>1);
+// console.log(2==1);
+// console.log(2 >= 2);
+// console.log(2 <= 1);
+
+console.log("1" <2);
+
+console.log(null>0);
+console.log(null==0);
+console.log(null<0);
+console.log(null<=0)
+console.log(null>=0)
+
+console.log(undefined>=0)
+console.log(undefined==0)
+console.log(undefined<0)
+
+// === (also chaeck datatypes)
+
+console.log("2"===2)
