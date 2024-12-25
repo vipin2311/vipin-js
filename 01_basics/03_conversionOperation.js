b4dbcf849719151b1847edcbbcdd3513951f5fca
vipin2311 <kumarvipin55512@gmail.com>
2024-12-25 15:08:01 +0000

    conversionOperation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -23,5 +23,38 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
  let number = 33
 
  let stringNumber =String(number)
- console.log(stringNumber);
- console.log(typeof stringNumber)
+//  console.log(stringNumber);
+//  console.log(typeof stringNumber) 
+
+// ***************************** OPERATION ************************************
+
+let value =3 
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2);
+
+let str1 = "hello"
+let str2 = " vipin"
+
+let str3 = str1 + str2
+//  console.log (str3);
+
+//  console.log(3+"1"+2);
+//  console.log("1"+2);
+//  console.log(7+2+"3");
+  
+
+console.log(+true);
+console.log(+"")
+
+num1 = num2 = num3 = 2+2
+
+let gamecounter = 100;
+++ gamecounter;
+console.log(gamecounter);
