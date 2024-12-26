a2c832b87fe64d2a0668d13cbd26b1818de5eb1d
vipin2311 <kumarvipin55512@gmail.com>
2024-12-26 15:13:31 +0000

    stringFunction.js

---
+++ 01_basics/05_string.js
@@ -0,0 +1,33 @@
+const name = "hitesh "
+const repoCount = 50
+//console.log(name +  repoCount + " values");
+
+console.log(`hello my name is ${name} and my repoCount is ${repoCount}`);
+
+const gameName = new String('vipinkumar')
+
+// console.log (gameName[5]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+
+console.log(gameName.charAt(8));
+console.log(gameName.indexOf('k'));
+
+const newString = gameName.substring(1,4)
+console.log(newString);
+
+const anotherString = gameName.slice(-5,1)
+console.log(anotherString);
+
+  const newStringOne = "     htesh "
+  console.log(newStringOne);
+  console.log(newStringOne.trim());
+
+  const url = "shdhsodsdskjas324"
+  console.log(url.replace('324','hayy'));
+
+  console.log(url.includes('324'));
+
+  console.log(gameName.split(''));
