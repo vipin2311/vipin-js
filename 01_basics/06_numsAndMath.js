b504902bc076afaaaefae372081384ef84ce181f
vipin2311 <kumarvipin55512@gmail.com>
2025-01-08 11:18:51 +0000

    math ans nums

---
+++ 01_basics/06_numsAndMath.js
@@ -0,0 +1,37 @@
+const score = 400;
+//console.log(score);
+
+const balance = new Number (100)
+//console.log(balance);
+
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(6));
+
+const otherNumber = 123.33
+
+//console.log(otherNumber.toPrecision(2));
+
+const hundreds= 1000000
+//console.log(hundreds.toLocaleString());
+//console.log(hundreds.toLocaleString('en-IN'));
+
+//+++++++++++++++++++++++m  Maths +++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(5.9));
+// console.log(Math.ceil(5.9));
+// console.log(Math.floor(5.9));
+// console.log(Math.max(5,9,4,7,2));
+// console.log(Math.min(5,9,4,7,2));
+
+console.log(Math.random())
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10;
+const max = 20
+
+console.log(Math.floor(Math.random()*(max-min +1))+min)
+
+
