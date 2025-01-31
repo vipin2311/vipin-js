d1184083dcacc4a8700e194db3fad81006bb5bb2
vipin2311 <kumarvipin55512@gmail.com>
2025-01-31 09:31:55 +0000

    switch

---
+++ 01_basics/switch.js
@@ -0,0 +1,30 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month= "jan"
+
+switch (month) {
+    case "jan":
+        console.log("january");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("random month");
+        
+        break;
+}
