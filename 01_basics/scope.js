5c1b920b790f60c1bfdc77991eab3c226e3d7997
vipin2311 <kumarvipin55512@gmail.com>
2025-01-31 09:30:46 +0000

    scope

---
+++ 01_basics/scope.js
@@ -0,0 +1,53 @@
+let a = 200
+if(true){
+    let a=2
+    const b = 23
+   // console.log("inner",a);
+    
+   
+}
+//console.log(a);
+//console.log(b);
+//console.log(a);
+
+
+
+function one(){
+    const username= "hitesh"
+    
+    function two (){
+        const website = "youtube"
+       // console.log(username);
+        
+    }
+    //console.log(website);
+
+    two()
+}
+
+//(one())
+
+if( true){
+    const username = "hitesh"
+
+    if(username=== "hitesh"){
+        const website = " youtube" 
+       // console.log(username+ website);  
+            
+        }
+
+    }
+
+  //  ++++++++++++++ intresting+++++++++++++++++++
+
+  function addone(num){
+    return num +1
+  }
+ console.log(addone(2));
+
+
+const addtwo = function(num){
+    return num +2
+}
+addtwo(4)
+
