57c7f897269a24c942952f1edb407574591205af
vipin2311 <kumarvipin55512@gmail.com>
2025-01-31 09:31:23 +0000

    one.js

---
+++ 01_basics/one.js
@@ -0,0 +1,60 @@
+//if
+// const isUSerLoggedIn = true
+// const temperature = 41
+
+// if(temperature===40){
+//     console.log("less than 50");
+// }
+// else{
+// console.log("temperature is greater than 50"); 
+// }          
+
+// console.log("execute");
+
+
+// if(3!="6"){
+//     console.log("executed");
+    
+// }
+// <,>, <=, >=, ==, !=, ===, !===
+
+// const score = 200
+
+// if (score>100){
+//     const power = "fly"
+//     console.log(`user power: ${power}`);
+    
+// }
+// console.log(`user power: ${power}`);
+
+const balance = 1000
+
+//if(balance>500) console.log("yes"),console.log("yes1"); don't do this type
+
+
+// if (balance <500){
+//     console.log("less than 500");
+// }else if (balance<750){
+//     console.log("less than 750");
+    
+// }
+// else if (balance <1200){
+//     console.log("less than 1400");
+    
+// }
+    
+const isUSerLoggedIn = true
+const debitCard = true
+const loggedInGoogle = false
+const loggedInEmail = true
+
+if (isUSerLoggedIn && debitCard && 2==2){
+    console.log("allow to buy course");
+    
+}
+if (loggedInGoogle ||loggedInEmail ){
+    console.log("user logged in");
+    
+}
+
+
