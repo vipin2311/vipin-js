0c0ed45040f3d117d1e4149a63f9a9a3b7f36f03
vipin2311 <kumarvipin55512@gmail.com>
2025-02-14 07:19:22 +0000

    forMapLops

---
+++ iterations/four.js
@@ -0,0 +1,28 @@
+const myObject = {
+    js:`javascript`,
+    cpp:`c++`,
+    rb:`ruby`,
+    swift: `swift by apple`
+}
+
+for (const key in myObject) {
+ //   console.log(`${key} shortcut is for $ {myObject[key]}` );  
+}
+
+const programming =["js",,"rb","java"]
+
+for (const key in programming) {
+   console.log(programming[key]);
+}
+
+// const map = new Map()
+
+// map.set(`IN`, "India")
+// map.set(`Fr`, "france")
+// map.set(`Pak`, "pakistan")
+
+// for (const key in map) {
+//     console.log(map[key]);
+    
+
+// }
